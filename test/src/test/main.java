5269e1e843e47b2d2814d4f2343151ceebd1d9e6
zerotestzero <leiwolong1515@yahoo.com>
2017-09-14 21:06:03 -0500

    init

---
+++ test/src/test/main.java
@@ -0,0 +1,15 @@
+package test;
+
+public class main {
+
+	public static void main(String[] args) {
+		int counter = 0;
+		
+		for(int i = 0; counter < 10; i++) {
+			System.out.print(counter + ", ");
+			counter++;
+		}
+		System.out.print(counter + ".");
+		
+	}
+}
