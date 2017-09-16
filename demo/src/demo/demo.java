67c0d0d2c2653ae9b7b66b40b352681f9b2a126c
zerotestzero <leiwolong1515@yahoo.com>
2017-09-16 14:53:55 -0500

    various

---
+++ demo/src/demo/demo.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class demo {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		// The eight primitive data types of java!
+		byte x = 0;
+		char c = 0; // 0-65300~
+		short s = 0;
+		int i = 0;
+		float f = 0.0f;
+		long L = 0;
+		double d = 0;
+		boolean b = false;
+		
+	}
+}
